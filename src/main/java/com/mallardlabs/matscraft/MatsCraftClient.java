218a2897d3e92a8c3d1c4e1d0a4be3a96921d243
William Pyke <william.pyke1@my.uwi.edu>
2024-09-23 00:37:46 -0400

    added CustomHudRenderer

--- src/main/java/com/mallardlabs/matscraft/MatsCraftClient.java
+++ src/main/java/com/mallardlabs/matscraft/MatsCraftClient.java
@@ -2,9 +2,13 @@ package com.mallardlabs.matscraft;
 
 import net.fabricmc.api.ClientModInitializer;
 
+import com.mallardlabs.matscraft.client.CustomHudRenderer;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+
 public class MatsCraftClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-
+        CustomHudRenderer hudRenderer = new CustomHudRenderer();
+        HudRenderCallback.EVENT.register(hudRenderer);
     }
 }
