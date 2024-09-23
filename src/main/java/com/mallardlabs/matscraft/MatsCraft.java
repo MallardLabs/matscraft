03bdd16ba32481451b69f0cc36786b3b5e4c139c
William Pyke <william.pyke1@my.uwi.edu>
2024-09-23 00:07:02 -0400

    add mats and matt luongo items

--- src/main/java/com/mallardlabs/matscraft/MatsCraft.java
+++ src/main/java/com/mallardlabs/matscraft/MatsCraft.java
@@ -1,5 +1,6 @@
 package com.mallardlabs.matscraft;
 
+import com.mallardlabs.matscraft.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -11,6 +12,6 @@ public class MatsCraft implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }
