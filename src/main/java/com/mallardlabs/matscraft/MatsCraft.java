00853544fcb85bb8ecaa22a6207bdfd864d19e77
WilliamPyke <pykewilliam1@gmail.com>
2024-12-15 18:55:59 -0400

    [RZ] Added matsCraft <> Nexus intercommunication and new mats item image

--- src/main/java/com/mallardlabs/matscraft/MatsCraft.java
+++ src/main/java/com/mallardlabs/matscraft/MatsCraft.java
@@ -1,12 +1,17 @@
 package com.mallardlabs.matscraft;
 
 import com.mallardlabs.matscraft.block.ModBlocks;
-import com.mallardlabs.matscraft.datagen.ModWorldGenerator;
+import com.mallardlabs.matscraft.config.ConfigManager;
 import com.mallardlabs.matscraft.item.ModItemGroups;
 import com.mallardlabs.matscraft.item.ModItems;
 import com.mallardlabs.matscraft.sound.ModSounds;
 import com.mallardlabs.matscraft.world.gen.ModWorldGeneration;
+import com.mallardlabs.matscraft.block.BlockBreakEvent;
+import com.mallardlabs.matscraft.commands.LinkAccount;
+import com.mallardlabs.matscraft.ws.WebSocketManager;
+
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,9 +25,14 @@ public class MatsCraft implements ModInitializer {
 		ModItemGroups.registerItemGroups();
 		ModItems.registerModItems();
 		ModBlocks.registerModBlocks();
-
 		ModSounds.registerSounds();
-
 		ModWorldGeneration.generateModWorldGeneration();
+		BlockBreakEvent.register();
+		WebSocketManager.initializeWebSocket(ConfigManager.getWebSocketUrl());
+
+        // Listener For Link Account Command
+		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, dedicated) -> {
+			LinkAccount.register(dispatcher);
+		});
 	}
 }
