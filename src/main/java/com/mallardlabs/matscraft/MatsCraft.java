54aae717ce04d7305e14be143c2a78a462dfd378
rz <neobyteid@gmail.com>
2024-12-23 01:08:22 +0700

    Update

--- src/main/java/com/mallardlabs/matscraft/MatsCraft.java
+++ src/main/java/com/mallardlabs/matscraft/MatsCraft.java
@@ -1,38 +1,45 @@
 package com.mallardlabs.matscraft;
 
 import com.mallardlabs.matscraft.block.ModBlocks;
-import com.mallardlabs.matscraft.config.ConfigManager;
+import com.mallardlabs.matscraft.events.ItemPickupEvent;
+import com.mallardlabs.matscraft.events.PlayerSpawnEvent;
 import com.mallardlabs.matscraft.item.ModItemGroups;
 import com.mallardlabs.matscraft.item.ModItems;
 import com.mallardlabs.matscraft.sound.ModSounds;
+import com.mallardlabs.matscraft.util.CustomNotification;
 import com.mallardlabs.matscraft.world.gen.ModWorldGeneration;
-import com.mallardlabs.matscraft.block.BlockBreakEvent;
+import com.mallardlabs.matscraft.events.BlockBreak;
 import com.mallardlabs.matscraft.commands.LinkAccount;
-import com.mallardlabs.matscraft.ws.WebSocketManager;
-
+import com.mallardlabs.matscraft.gui.MatsBalanceOverlay;
 import net.fabricmc.api.ModInitializer;
+
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class MatsCraft implements ModInitializer {
+public class MatsCraft implements ModInitializer{
 	public static final String MOD_ID = "matscraft";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
-	public void onInitialize() {
+    public void onInitialize(){
 		ModItemGroups.registerItemGroups();
 		ModItems.registerModItems();
 		ModBlocks.registerModBlocks();
 		ModSounds.registerSounds();
 		ModWorldGeneration.generateModWorldGeneration();
-		BlockBreakEvent.register();
-		WebSocketManager.initializeWebSocket(ConfigManager.getWebSocketUrl());
+		BlockBreak.register();
+		ItemPickupEvent.register();
+		PlayerSpawnEvent.register();
+		MatsBalanceOverlay matsBalanceOverlay = new MatsBalanceOverlay();
+		matsBalanceOverlay.initialize();
+		CustomNotification.register();
+		 // Tampilkan selama 100 tick
 
-        // Listener For Link Account Command
 		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, dedicated) -> {
 			LinkAccount.register(dispatcher);
 		});
 	}
+
 }
